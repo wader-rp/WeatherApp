651a75cb987d4a3d02c6e412ca200cf14f4c8602
wader-rp <waddder@gmail.com>
2022-03-25 22:11:41 +0100

    netlify

--- src/components/WeatherContainer/FiveDaysForecastBox/FiveDaysForecastBox.jsx
+++ src/components/WeatherContainer/FiveDaysForecastBox/FiveDaysForecastBox.jsx
@@ -7,13 +7,13 @@ import { handleSelect } from "../../../helpers/handleSelect";
 
 const FiveDaysForecastBox = ({ weatherData, activeData, setActiveData }) => {
   const [fiveDayForecast, setFiveDayForecast] = useState([]);
-  const API_KEY_FIVE_DAYS = "958c2a7e21f1fdde6aea565becbd9aa4";
+  const API_KEY_FIVE_DAYS_FORECAST = "958c2a7e21f1fdde6aea565becbd9aa4";
   const { lat, lon } = weatherData.coord;
 
   useEffect(() => {
     let sub = true;
     fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly,alerts&appid=${API_KEY_FIVE_DAYS}`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly,alerts&appid=${API_KEY_FIVE_DAYS_FORECAST}`
     )
       .then((response) => response.json())
       .then((data) => {
